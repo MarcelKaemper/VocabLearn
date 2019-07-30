accee3e81f4b609d6f1ee9208856dcb5925949c6
Marcel <marcelkaemper862@gmail.com>
2019-07-30 17:44:59 +0200

    Added function to write data into sqlite db

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
@@ -5,27 +6,21 @@
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName);
 char* readString(void);
+void insertIntoDB(char* word1, char* word2);
 
 int main(int argc, char ** argv) {
+
 
     initscr();
 
     int h,w;
     getmaxyx(stdscr, h, w);
 
-	/* // DB STUFF */
-	/* sqlite3 *db; */
-	/* int rc; */
-	/* rc = sqlite3_open("db", &db); */
-	/* char *err; */
-	/* char *sql = "INSERT INTO words(word,word2) VALUES ('tset', 'test2')"; */
-	/* sqlite3_exec(db,sql,callback, 0, &err); */
-	/* sqlite3_close(db); */
     
     WINDOW *win = newwin(h,w,0,0);
 
-	char * word1=NULL;
-	char * word2=NULL;
+	char * word1 = NULL;
+	char * word2 = NULL;
 
 
     noecho();
@@ -36,8 +31,8 @@ int main(int argc, char ** argv) {
 
     endwin();
 
-	printf("%s\n%s", word1, word2);
-
+	/* printf("%s\n%s", word1, word2); */
+	insertIntoDB(word1,word2);
 
     return 0;
 }
@@ -70,3 +65,16 @@ char* readString(void){
 }
 
 
+void insertIntoDB(char* word1, char* word2){
+	int rc;
+	char *err;
+	char *sql;
+
+	sqlite3 *db;
+	rc = sqlite3_open("db", &db);
+
+	asprintf(&sql,"INSERT INTO words(word,word2) VALUES ('%s', '%s')",word1,word2);
+
+	sqlite3_exec(db,sql,callback, 0, &err);
+	sqlite3_close(db);
+}
